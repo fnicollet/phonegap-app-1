88ca08e7dee78aa7eb126fa4ee9fa2d1a05e281a
Fabien NICOLLET <fnicollet@gmail.com>
2016-01-16 17:07:32 +0100

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -3,25 +3,19 @@ var app = {
     initialize: function() {
         this.bindEvents();
     },
-    // Bind Event Listeners
-    //
     // Bind any events that are required on startup. Common events are:
     // 'load', 'deviceready', 'offline', and 'online'.
     bindEvents: function() {
         document.addEventListener('deviceready', this.onDeviceReady, false);
     },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
         app.receivedEvent('deviceready');
         // Ignore back button in android
         // document.addEventListener('backbutton', function() {}, false);
         var loading = document.getElementById("loading");
-        loading.setAttribute('style', 'display:none;');
+        loading.style.display = 'none';
         var iframeComponent = document.getElementById("iframeComponent");
-        iframeComponent.setAttribute('style', 'display:"";');
+        iframeComponent.style.display = '';
         
     }
 };
