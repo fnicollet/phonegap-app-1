bcdc679a0c6ea57596dd9cc33317cf948025cedb
Fabien NICOLLET <fnicollet@gmail.com>
2016-03-25 16:06:48 +0100

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -9,24 +9,27 @@ var app = {
         document.addEventListener('deviceready', this.onDeviceReady, false);
     },
     onDeviceReady: function() {
+    	document.getElementById("debugDiv").innerHTML = "onDeviceReady";
+            window.addEventListener("message", function(event) {
+	document.getElementById("debugDiv").innerHTML = "on message";
+	    
+		var data = event.data;
+		var action = data.action;
+		document.getElementById("debugDiv").innerHTML += action;
+		if(action == "LINK") {
+			var parameters = data.parameters;
+			var href = parameters[0];
+			document.getElementById("debugDiv").innerHTML += href;
+			window.open(href, "_system");
+		}
+	}, false);
+    	
         app.receivedEvent('deviceready');
         // http://docs.phonegap.com/en/2.5.0/cordova_splashscreen_splashscreen.md.html#hide
         navigator.splashscreen.hide();
         // Ignore back button in android
         // document.addEventListener('backbutton', function() {}, false);
          document.getElementById("debugDiv").innerHTML = "TEST";
-        window.addEventListener("message", function(event) {
-        	document.getElementById("debugDiv").innerHTML = "on message";
-		    
-			var data = event.data;
-			var action = data.action;
-			document.getElementById("debugDiv").innerHTML += action;
-			if(action == "LINK") {
-				var parameters = data.parameters;
-				var href = parameters[0];
-				document.getElementById("debugDiv").innerHTML += href;
-				window.open(href, "_system");
-			}
-		}, false);
+
     }
 };
